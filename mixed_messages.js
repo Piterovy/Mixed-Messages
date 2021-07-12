815f6fd9a6dc79d49ae99aa5809e5256885c8c40
Piotr Orczykowski <piterovy95@gmail.com>
2021-07-12 22:02:44 +0100

    Added arrays storing all random parts of mixed message.

---
+++ mixed_messages.js
@@ -0,0 +1,20 @@
+const personList = ['your mom', 'your dad',
+                    'your brother', 'your sister',
+                    'your grandmother', 'your grandfather',
+                    'your girlfriend', 'your cousin',
+                    'your boyfriend', 'your sugar daddy'];
+
+const famousPersonList = ['the Holy Roman Emperor', 'the Pope',
+                        'Genghis Khan', 'the Last Samurai',
+                        'the Last Jedi', 'Marco Polo',
+                        'Chopin', 'president of the USA',
+                        'Boris Johnson\'s great-grandmother',
+                        'Ezio Auditore da Firenze', 'Charlemagne'];
+
+const activityList = ['sleeping', 'hiking', 'playing poker',
+                    'playing League of Legends', 'assembling gaming PCs',
+                    'drawing genitalia', 'doing drugs', 'partying',
+                    'dancing', 'playing cards'];
+
+const placeList = ['Paris', 'Mordor', 'London', 'Berlin', 'Birmingham',
+                'your room', 'Venice', 'Japan', 'Chechenya', 'Los Angeles', 'New York'];
